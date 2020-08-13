42439955ec274c482274e621999e62dd98cda39e
Ravi Kumar <ravichouhan00007@gmail.com>
2020-08-13 19:43:19 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const multer = require('multer');
+const bodyparse = require('body-parser');
+const path = require('path');
+const { nextTick } = require('process');
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant')
+
+
+const app = express()
+
+
+app.use('/uploads',express.static(path.join(__dirname + 'uploads')));
+app.set('view engine','ejs');
+app.use(bodyparse.json());
+app.use(bodyparse.urlencoded({extended:true}));
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"uploads")
+    },
+    filename:function(req,file,cb){
+        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+
+    }
+}) 
+
+const upload = multer({
+    storage:storage
+})
+
+app.get('/',(req,res)=>{
+    res.render("index")
+})       
+                 
+
+app.post('/',upload.single('image'),(req,res,next)=>{
+    const file = req.file
+    var ext
+    if(!file){
+        const error = new Error('Please upload a file')
+        error.httpStatusCoad= 404
+        return next(error)
+    }
+    if(file.mimetype=="image/jpeg"){
+       ext = "jpeg"
+    }
+    if(file.mimetype == "image/png"){
+        ext = "png"
+    }
+    res.render('image',{url:file.path,name:file.filename,ext:ext})
+})
+
+app.post('/compress/uploads/:name/:ext',async(req,res)=>{
+  const files = await imagemin(["uploads/" + req.params.name],{
+      destination:"output",
+      plugins: [
+          imageminJpegtran(),
+          imageminPngquant({
+              quality:[0.6,0.8]
+          })
+      ]
+  })
+
+  // download the image
+  res.download(files[0].destinationPath)
+})
+
+app.listen(7000,()=>{
+    console.log('Server Started at port 7000');
+})
+
+
+
+
